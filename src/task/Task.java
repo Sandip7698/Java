0203ce0cb0f90ab08709736e9c69e95a4a5a2a6c
Sandip7698 <sandip.jadhav@indicchain.com>
2022-11-04 15:58:39 +0530

    reverse and 2nd last position

--- src/task/Task.java
+++ src/task/Task.java
@@ -8,34 +8,40 @@ public class Task {
         Queue<Integer> queue=new LinkedList<>();
         Stack<Integer> stack = new Stack<Integer>();
         List<Integer> list=new ArrayList<Integer>();
-        System.out.println("Eneter Size = ");
+        System.out.println("Enetr Size = ");
         int size = sc.nextInt();
 
-        System.out.println("************************************************");
+
         System.out.println("Eneter Queue Elements");
         for(int i=0;i<size;i++){
             queue.add(sc.nextInt());
         }
 
-        System.out.println("************************************************");
+
         System.out.println("Elements of Queue");
 
         System.out.println(queue);
 
-        System.out.println("************************************************");
+
         System.out.println("Eneter stack Elements");
         for (int j = 0; j < size; j++) {
             stack.push(sc.nextInt());
         }
-        System.out.println("************************************************");
+
         System.out.println("Elements of Stack");
         System.out.println(stack);
 
-        System.out.println("************************************************");
+
         System.out.println("Sum of the Queue and Stack");
 
 
+        System.out.println("Reverse of List");
+        Collections.reverse(stack);
+        System.out.println(stack);
 
+        System.out.println("2nd Last position Element");
+        int last=stack.get(stack.size()-2);
+        System.out.println(last);
 
     }
 }
