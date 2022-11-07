fe6d0eb682f19d60e75e8fa1c825729e621a4f5f
Sandip7698 <sandip.jadhav@indicchain.com>
2022-11-07 18:33:26 +0530

    task2

--- src/task/Task2.java
+++ src/task/Task2.java
@@ -8,20 +8,32 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        HashMap<Integer,String> map=new HashMap<Integer,String>();
-        ArrayList li=new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        ArrayList li = new ArrayList();
         System.out.println("Enter the Size =");
-        int size=sc.nextInt();
-        for (int i=1;i<=size;i++){
-                System.out.println("Eneter the Name : ");
-                String name = sc.next();
-                map.put(i, name);
+        int size = sc.nextInt();
+        String name;
+        int key;
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <=3; j++) {
+                System.out.println("Enter the Key : ");
+                 key = sc.nextInt();
+                System.out.println("Enter the Name : ");
+                name = sc.next();
+                map.put(key, name);
+                System.out.println(map);
 
+            }
+
+            li.add(map);
+//            System.out.println(li);
         }
-        li.add(map);
         System.out.println(li);
 
 
+
+
+        }
     }
-}
+
